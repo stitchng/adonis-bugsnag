e6c82cca32135152f4d4dc6fb0352d8be79f2421
StitchNG <stitchnig@gmail.com>
2019-01-02 14:21:19 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/**
+ * adonis-bugsnag
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com> | Aziz Abdul <>
+ */
+
+const path = require('path')
+
+module.exports = async function (cli) {
+  await cli.makeConfig('bugsnag.js', path.join(__dirname, './config/bugsnag.js'))
+    .catch((e) => {})
+    
+  cli.command.completed('create', 'config/bugsnag.js')
+}
