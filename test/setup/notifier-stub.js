06c245ad3ce8c6ff5498fcf5520dd7d90c4e4c93
StitchNG <stitchnig@gmail.com>
2019-03-03 13:28:41 +0100

    Update notifier-stub.js

--- test/setup/notifier-stub.js
+++ test/setup/notifier-stub.js
@@ -1,4 +1,11 @@
 
 module.exports = {
-
+    notify:function(error, options){
+        this.notified = true
+    },
+    startSession:function(){
+        this.sessionStarted = true
+    },
+    notified: false,
+    sessionStarted: false
 }
