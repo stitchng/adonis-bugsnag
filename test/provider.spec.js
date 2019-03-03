3e04a096c33aafb2e3607b9642354a65b2ee421c
StitchNG <stitchnig@gmail.com>
2019-03-03 12:47:06 +0100

    Update provider.spec.js

--- test/provider.spec.js
+++ test/provider.spec.js
@@ -1,1 +1,20 @@
+'use strict'
 
+/*
+ * adonis-bugsnag
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+const test = require('japa')
+const { Config, Helpers } = require('@adonisjs/sink')
+const BugSnag = require('../src/providers/BugSnagProvider.js')
+
+test.group('Adonis BugSnag Test(s)', (group) => {
+  group.beforeEach(() => {
+    this.helpers = new Helpers(path.join(__dirname, './'))
+  })
+})
