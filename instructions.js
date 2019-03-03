07c4d1abc3dfe4c4a727e242fb9fbaf05ef04579
StitchNG <stitchnig@gmail.com>
2019-03-03 15:42:44 +0100

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -11,7 +11,7 @@
 const path = require('path')
 
 module.exports = async function (cli) {
-  await cli.makeConfig('bugsnag.js', path.join(__dirname, './config/bugsnag.js'))
+  await cli.makeConfig('bugsnag.js', path.join(__dirname, 'config/bugsnag.js'))
     .catch((e) => {})
     
   cli.command.completed('create', 'config/bugsnag.js')
