c0ba462cb24ec44ca9df6775a25e160597bcef42
StitchNG <stitchnig@gmail.com>
2019-03-03 13:05:25 +0100

    Update and rename provider.spec.js to bugsnagnotifier.spec.js

--- test/bugsnagnotifier.spec.js
+++ test/bugsnagnotifier.spec.js
@@ -11,10 +11,20 @@
 
 const test = require('japa')
 const { Config, Helpers } = require('@adonisjs/sink')
-const BugSnag = require('../src/providers/BugSnagProvider.js')
+const BugSnag = require('../src/index.js')
+const BugSnagJSNotifierStub = require('setup/notifier-stub.js')
+
 
 test.group('Adonis BugSnag Test(s)', (group) => {
   group.beforeEach(() => {
     this.helpers = new Helpers(path.join(__dirname, './'))
+    this.config = new Config()
+    this.env = new Env()
+  })
+  
+  test('instantiate without errors or side-effects', () => {
+    const providerInstance = new BugSnag(BugSnagJSNotifierStub, this.config, this.helpers, this.env)
+
+    //assert.
   })
 })
