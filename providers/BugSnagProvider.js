3729b76f5c9c535e1b2a9cde8cf7a15fd87a447d
StitchNG <stitchnig@gmail.com>
2019-03-03 13:24:14 +0100

    Update BugSnagProvider.js

--- providers/BugSnagProvider.js
+++ providers/BugSnagProvider.js
@@ -6,9 +6,14 @@ class BugSnagProvider extends ServiceProvider {
   register () {
     this.app.singleton('Adonis/Addons/BugSnag', () => {
       const Config = this.app.use('Adonis/Src/Config')
+      const Helpers = this.app.use('Helpers')
+      const Env = this.app.use('Env')
       const BugSnag = require('../src/BugSnag')
-      return new BugSnag(require('@bugsnag/js'), Config)
+      
+      return new BugSnag(require('@bugsnag/js'), Config, Helpers, Env)
     })
+    
+    this.app.bind('Adonis/Middleware/BugSnagUser', () => require('../src/BugSnag/Middleware/BugSnagUser'))
   }
 }
 
