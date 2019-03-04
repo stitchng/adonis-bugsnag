d4e6a46e2030d8c21dc009d3990165b23be0a728
Ifeora Okechukwu <isocroft@gmail.com>
2019-03-04 09:01:53 +0100

    stub file fix

--- test/setup/notifier-stub.js
+++ test/setup/notifier-stub.js
@@ -1,10 +1,14 @@
-module.exports = {
-  notify: function () {
-    this.notified = true
-  },
-  startSession: function () {
-    this.sessionStarted = true
-  },
-  notified: false,
-  sessionStarted: false
+const Notifier = function () {
+  return {
+    notify: function () {
+      this.notified = true
+    },
+    startSession: function () {
+      this.sessionStarted = true
+    },
+    notified: false,
+    sessionStarted: false
+  }
 }
+
+module.exports = Notifier
