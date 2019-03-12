9d6b1c64dedae24f0a246341fdc72de0d9f78b8b
StitchNG <stitchnig@gmail.com>
2019-03-12 14:00:50 +0100

    Update BugSnagUser.js

--- src/BugSnag/Middleware/BugSnagUser.js
+++ src/BugSnag/Middleware/BugSnagUser.js
@@ -5,18 +5,20 @@ class BugSnagUser {
     this.notifierWrapper = bugsnag
   }
 
-  async handle ({ request, auth }, next) {
+  async handle ({ request, session, auth }, next) {
     
     try{
       let _user = await (typeof (auth.getUser) === 'function' ? auth.getUser() : Promise.resolve(null))
 
-      this.notifierWrapper.notifierWrapper.setAuthUser(_user)
-      this.notifierWrapper.setContext()
+      this.notifierWrapper.setAuthUser(_user)
+      this.notifierWrapper.setContext(request, session)
     }catch(err){
       ;
     }
 
     await next()
+    
+    // this.notifierWrapper.leaveBreadcrumb('"name"', '{metaData}', '"type"', 'timestamp')
   }
 }
 
